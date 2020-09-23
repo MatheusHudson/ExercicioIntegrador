93a0fa371b0591ec810796c9f6b4fa4faed945d0
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2020-09-23 20:48:19 -0300

    Livros incializados

---
+++ src/br/exercicioIntegrador/com/Livro.kt
@@ -0,0 +1,5 @@
+
+class Livro(var titulo:String,var autor:String,var anoLancamento:Int,
+            var qdtEstoque:Int,var preco:Double) {
+    var codigo:Int = 0
+}
--- src/br/exercicioIntegrador/com/Main.kt
+++ src/br/exercicioIntegrador/com/Main.kt
@@ -1,8 +1,43 @@
 package br.exercicioIntegrador.com
 
+import Livro
+
 fun main() {
 
-    val livro1 = Livro()
+
+
+    val livro1 = Livro("O Senhor dos Anéis: A Sociedade do Anel ",
+            " Peter Jackson",2001,65,89.90)
+
+    val livro2 = Livro("O Senhor dos Anéis: As Duas Torres ",
+            " Peter Jackson",2002,35,94.90)
+
+    val livro3 = Livro("O Senhor dos Anéis: O Retorno do Rei ",
+            " Peter Jackson",2003,25,99.90)
+
+    val livro4 = Livro(" Harry Potter e a Pedra Filosofal ",
+            "  J.K. Rowling",2001,1,72.90)
+
+    val livro5 = Livro(" Harry Potter e a Câmara Secreta ",
+            "  J.K. Rowling",2002,91,74.90)
+
+    val livro6 = Livro(" Harry Potter e o Prisioneiro de Azkaban ",
+            "  J.K. Rowling",2003,14,75.90)
+
+    val livro7 = Livro(" Harry Potter e o Cálice de Fogo ",
+            "  J.K. Rowling",2004,13,76.90)
+
+    val livro8 = Livro("  Harry Potter e a Ordem da Fênix ",
+            "  J.K. Rowling",2001,31,78.90)
+
+    val livro9 = Livro("   Maze Runner Correr ou Morrer ",
+            "  James Dashner",2009,41,99.90)
+
+    val livro10 = Livro("  Maze Runner Prova de Fogo ",
+            "  James Dashner",2010,0,92.90)
+
+
+
 
 
 }
